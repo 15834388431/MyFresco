004721d082a279800ff182a434c1c6c8b1303b8d
845429353 <845429353@qq.com>
2016-09-19 19:20:38 +0800

    Initial commit

---
+++ app/src/main/java/com/xin/myfresco/MyApplication.java
@@ -0,0 +1,24 @@
+package com.xin.myfresco;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.decoder.SimpleProgressiveJpegConfig;
+
+/**
+ * Created by john on 2016/9/19.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        //基本使用的初始化方法
+//        Fresco.initialize(this);
+
+        //渐进式JPEG图的初始化方法
+        ImagePipelineConfig config = ImagePipelineConfig.newBuilder(this)
+                .setProgressiveJpegConfig(new SimpleProgressiveJpegConfig()).build();
+        Fresco.initialize(this, config);
+    }
+}
